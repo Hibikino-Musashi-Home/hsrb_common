1b0482a0e45f6c8f9c719a0287c376c31ca2d334
koji_terada <koji_terada@mail.toyota.co.jp>
2018-01-17 18:16:14 +0900

    Stabilize gripper control.
    - light weight dummy mass.
    - fasten control period.
    - update dumping factor(for sim only)
    - set max_torque

--- hsrb_description/test/test_urdf.py
+++ hsrb_description/test/test_urdf.py
@@ -24,7 +24,7 @@ def test_generator_robot_urdf():
     def test_robot_urdf(path):
         u"""XACROで変換した後に、URDFとして正しく読めるかを確認するテスト"""
         with tempfile.NamedTemporaryFile() as f:
-            args = ['rosrun', 'xacro', 'xacro', '--inorder',  source]
+            args = ['rosrun', 'xacro', 'xacro', '--inorder', source]
             eq_(subprocess.call(args, stdout=f), 0)
             args = ['check_urdf', f.name]
             subprocess.check_output(args)
