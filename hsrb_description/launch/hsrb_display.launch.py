a786152987d22882bbd2905e5fcb67ac5b48a7dd
Keisuke Takeshita <keisuke_takeshita@mail.toyota.co.jp>
2022-03-22 16:26:10 +0900

    Port hsrc_description to ROS2

--- hsrb_description/launch/hsrb_display.launch.py
+++ hsrb_description/launch/hsrb_display.launch.py
@@ -1,49 +1,48 @@
 from launch import LaunchDescription
+
 from launch.actions import DeclareLaunchArgument
-from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution
+from launch.substitutions import (
+    Command,
+    FindExecutable,
+    LaunchConfiguration,
+    PathJoinSubstitution,
+)
 
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
 
 
+def declare_arguments():
+    declared_arguments = []
+    declared_arguments.append(
+        DeclareLaunchArgument('description_package',
+                              default_value='hsrb_description',
+                              description='Description package with robot URDF/xacro files.'))
+    declared_arguments.append(
+        DeclareLaunchArgument('description_file',
+                              default_value='hsrb4s.urdf.xacro',
+                              description='URDF/XACRO description file with the robot.'))
+    return declared_arguments
+
+
 def generate_launch_description():
+    description_package = LaunchConfiguration('description_package')
+    description_file = LaunchConfiguration('description_file')
     robot_description_content = Command(
-        [
-            PathJoinSubstitution([FindExecutable(name="xacro")]),
-            " ",
-            PathJoinSubstitution(
-                [FindPackageShare("hsrb_description"), "robots", "hsrb4s.urdf.xacro"]
-            ),
-        ]
-    )
-    robot_description = {"robot_description": robot_description_content}
+        [PathJoinSubstitution([FindExecutable(name='xacro')]), ' ',
+         PathJoinSubstitution([FindPackageShare(description_package), 'robots', description_file])])
 
-    rviz_config_file = PathJoinSubstitution(
-        [FindPackageShare("hsrb_description"), "launch", "hsrb_display.rviz"]
-    )
+    robot_description = {'robot_description': robot_description_content}
+    rviz_config_file = PathJoinSubstitution([FindPackageShare('hsrb_description'), 'launch', 'display.rviz'])
 
-    joint_state_publisher_node = Node(
-        package="joint_state_publisher_gui",
-        executable="joint_state_publisher_gui",
-    )
-    robot_state_publisher_node = Node(
-        package="robot_state_publisher",
-        executable="robot_state_publisher",
-        output="both",
-        parameters=[robot_description],
-    )
-    rviz_node = Node(
-        package="rviz2",
-        executable="rviz2",
-        name="rviz2",
-        output="log",
-        arguments=["-d", rviz_config_file],
-    )
+    joint_state_publisher_node = Node(package='joint_state_publisher_gui',
+                                      executable='joint_state_publisher_gui')
+    robot_state_publisher_node = Node(package='robot_state_publisher',
+                                      executable='robot_state_publisher',
+                                      parameters=[robot_description])
+    rviz_node = Node(package='rviz2',
+                     executable='rviz2',
+                     arguments=['-d', rviz_config_file])
 
-    nodes = [
-        joint_state_publisher_node,
-        robot_state_publisher_node,
-        rviz_node,
-    ]
-
-    return LaunchDescription(nodes)
+    nodes = [joint_state_publisher_node, robot_state_publisher_node, rviz_node]
+    return LaunchDescription(declare_arguments() + nodes)
